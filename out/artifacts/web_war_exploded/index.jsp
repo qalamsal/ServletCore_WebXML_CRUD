18dc9c660df6a4a1239e72daa95607175c4315b4
qalamsal8@gmail.com <qalamsal8@gmail.com>
2021-03-17 10:56:27 -0500

    initial commit

---
+++ out/artifacts/web_war_exploded/index.jsp
@@ -0,0 +1,13 @@
+<%-- Created by IntelliJ IDEA. --%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+  <h1>WELCOME TO J2EE</h1>
+
+  <a href="login.jsp">login</a>
+
+  </body>
+</html>
---
+++ src/servlets/LoginServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("GET METHOD");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("Form Posted");
+        String user=req.getParameter("email-address");
+        //System.out.println(user);
+        String pass=req.getParameter("password");
+        boolean isUserValid=UserService.verifyLogin(user,pass);
+    }
+}
